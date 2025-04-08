3211d56f837e0c255ba0da75aaeebe7560bd8dfd
MoARABY <arabym702@gmail.com>
2025-04-08 22:50:00 +0200

    ecommerce schema implement

--- server.js
+++ server.js
@@ -5,17 +5,73 @@ const {buildSchema} = require('graphql')
 
 
 const schema = buildSchema(`
-type Query {
-    description:String
-    price:Float
-}`
+    type Query {
+        products:[Products]
+        orders:[Orders]
+    }
+    type Products {
+        id:ID
+        description:String
+        price:Float
+        reviews:[Reviews]
+    }
+    type Orders {
+        id:ID
+        date:String
+        subTotal:Float
+        items:[OrderItems]
+    }
+    type OrderItems {
+        id:ID
+        product:Products
+        quantity:Float
+    }
+    type Reviews {
+        id:ID
+        rating:Int
+        comment:String
+    }
+`
 )
 
 
 // مصدر الداتا
 const root = {
-    description:'red shoes',
-    price:22.13
+    products : [
+        {
+            id:'123',
+            description:'item one',
+            price: 22
+        },
+        {
+            id:'122',
+            description:'item two',
+            price: 40
+        }
+    ],
+    orders:[
+        {
+            id:'111',
+            date:'20-2-2025',
+            subTotal:40,
+            items:[
+                {
+                    product : {
+                        id:'123',
+                        description:'newww'
+                    },
+                    quantity:4
+                },
+                {
+                    product : {
+                        id:'122',
+                        description:'oldddd'
+                    },
+                    quantity:3
+                }
+            ]
+        }
+    ]
 }
 
 const app = express()
