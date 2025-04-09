94d26e1c582a8444f410ac72e4cd48e287b8d126
MoARABY <arabym702@gmail.com>
2025-04-09 13:49:16 +0200

    apply parametarized resolvers query params

--- products/products.model.js
+++ products/products.model.js
@@ -17,5 +17,11 @@ const products = [
 const getAllProducts = () => {
     return products
 }
+const getProductById = (id) =>{
+    return products.find(product =>(product.id === id))
+}
+const getProductsByPrice = (min,max) => {
+    return products.filter(product => product.price >= min && product.price <= max)
+}
 
-module.exports = {getAllProducts}
+module.exports = {getAllProducts,getProductsByPrice,getProductById}
--- products/products.resolver.js
+++ products/products.resolver.js
@@ -5,6 +5,12 @@ module.exports = {
     Query :{
         products: () => {
             return  ProductModel.getAllProducts()
+        },
+        productsByPrice: (parent,args) => {
+            return ProductModel.getProductsByPrice(args.min,args.max)
+        },
+        productById : (_,args) =>{
+            return ProductModel.getProductById(args.id)
         }
     }
 }
